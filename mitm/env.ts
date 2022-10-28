c23df26f699b6fc81bd95815723aae483d5556c3
Blake Byrnes <blakebyrnes@gmail.com>
2022-10-28 13:35:41 -0400

    fix(mitm): replace paths in env actual vars

--- mitm/env.ts
+++ mitm/env.ts
@@ -1,10 +1,11 @@
-import * as Os from 'os';
 import { loadEnv, parseEnvBool, parseEnvPath } from '@ulixee/commons/lib/envUtils';
 
 loadEnv(__dirname);
 
 const env = process.env;
 const envDebug = env.DEBUG ?? '';
+if(env.UBK_DATA_DIR) env.UBK_DATA_DIR = parseEnvPath(env.UBK_DATA_DIR);
+if(env.UBK_NETWORK_DIR) env.UBK_NETWORK_DIR = parseEnvPath(env.UBK_NETWORK_DIR);
 
 export default {
   sslKeylogFile: env.SSLKEYLOGFILE,
@@ -13,9 +14,7 @@ export default {
   // https://github.com/golang/go/issues/38365
   allowInsecure: parseEnvBool(env.UBK_MITM_ALLOW_INSECURE),
   enableMitmCache: parseEnvBool(env.UBK_MITM_ENABLED_CACHE),
-  defaultStorageDirectory: parseEnvPath(
-    (env.UBK_NETWORK_DIR ?? env.UBK_DATA_DIR)?.replace('<TMP>', Os.tmpdir()),
-  ),
+  defaultStorageDirectory: env.UBK_NETWORK_DIR ?? env.UBK_DATA_DIR,
   isDebug:
     envDebug.includes('ubk:*') ||
     envDebug.includes('ubk*') ||
